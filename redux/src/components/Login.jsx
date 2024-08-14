d696dccd2c561145148e902d2eb060419b3d50ae
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-14 15:42:13 +0200

    final rudcer with login logic

--- redux/src/components/Login.jsx
+++ redux/src/components/Login.jsx
@@ -1,9 +1,18 @@
-import React from "react";
-
+import { useDispatch } from "react-redux";
+import { login } from "../features/User";
 function Login() {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button>Login</button>
+      <button
+        onClick={() => {
+          dispatch(
+            login({ name: "Nyiringango", age: 24, email: "ntayo bro " })
+          );
+        }}
+      >
+        Login
+      </button>
     </div>
   );
 }
--- redux/src/features/User.js
+++ redux/src/features/User.js
@@ -3,9 +3,10 @@ export const userSlice = createSlice({
   name: "USER",
   initialState: { value: { name: "kevin", age: 30, email: "kevin@gmail.com" } },
   reducers: {
-    Login: (state, action) => {
+    login: (state, action) => {
       state.value = action.payload;
     },
   },
 });
+export const { login } = userSlice.actions;
 export default userSlice.reducer;
